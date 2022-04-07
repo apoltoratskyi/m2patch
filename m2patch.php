206acc8e9b0b24da09bdcf765eae4733e36928d0
Andrii Poltoratskyi <poltorat@adobe.com>
2022-04-06 21:10:39 -0700

    added bundle repositories

--- m2patch.php
+++ m2patch.php
@@ -1,37 +1,105 @@
 #!/usr/bin/env php
 <?php
 
-function replaceCallback($matches)
-{
-    global $replaceMap;
-    if (empty($matches[1]) || empty($matches[2])) {
-        return $matches[0];
-    }
-
-    if (preg_match('/app\/design/', $matches[1])) {
-        $a = 0;
-    }
-
-    $path = $matches[1];
-    $module = $matches[2];
-
-    if (empty($replaceMap[$path])) {
-        return $matches[0];
-    }
-
-    preg_match_all('/[A-Z][a-z]+/', $module, $modulePartsRaw);
-
-    if (!empty($modulePartsRaw[0])) {
-        $moduleParts = $modulePartsRaw[0];
-        $moduleName = implode('-', $moduleParts);
-    } else {
-        $moduleName = $module;
-    }
-
-    $modulePath = $replaceMap[$path] . strtolower($moduleName) . '/';
-
-    return  $modulePath;
-}
+$repoPath = '/Users/poltorat/PhpstormProjects/m2p';
+$repositories = [
+    ['ce',               'git@github.com:magento-sparta/magento2ce.git'],
+    ['ee',               'git@github.com:magento-sparta/magento2ee.git'],
+    ['b2b',               'git@github.com:magento-sparta/magento2b2b.git'],
+    ['inventory',         'git@github.com:magento-sparta/inventory.git'],
+    ['pb',                'git@github.com:magento-sparta/magento2-page-builder.git'],
+    ['security',          'git@github.com:magento-sparta/security-package.git']
+];
+$mapping =
+    ['b2b'=>
+        ['2.3.1'=>    '1.1.1',
+            '2.3.2'=>    '1.1.2',
+            '2.3.2-p2'=> '1.1.2-p2',
+            '2.3.3'=>    '1.1.3',
+            '2.3.3-p1'=> '1.1.3-p1',
+            '2.3.4'=>    '1.1.4',
+            '2.3.4-p1'=> '1.1.4-p1',
+            '2.3.4-p2'=> '1.1.4-p2',
+            '2.3.5'=>    '1.1.5',
+            '2.3.5-p1'=> '1.1.5-p1',
+            '2.3.5-p2'=> '1.1.5-p2',
+            '2.3.6'=>    '1.1.6',
+            '2.3.6-p1'=> '1.1.6-p1',
+            '2.3.7'=>    '1.1.7',
+            '2.3.7-p1'=> '1.1.7-p1',
+            '2.3.7-p2'=> '1.1.7-p2',
+            '2.4.0'=>    '1.2.0',
+            '2.4.0-p1'=> '1.2.0-p1',
+            '2.4.1'=>    '1.3.0',
+            '2.4.1-p1'=> '1.3.0-p1',
+            '2.4.2'=>    '1.3.1',
+            '2.4.2-p1'=> '1.3.1-p1',
+            '2.4.2-p2'=> '1.3.1-p2',
+            '2.4.3'=>    '1.3.2',
+            '2.4.3-p1'=> '1.3.2-p1'],
+        'inventory' =>
+            ['2.3.1'=>   '1.0.3',
+                '2.3.2'=>    '1.1.2',
+                '2.3.2-p2'=> '1.1.2',
+                '2.3.3'=>    '1.1.3',
+                '2.3.3-p1'=> '1.1.3',
+                '2.3.4'=>    '1.1.4',
+                '2.3.4-p1'=> '1.1.4',
+                '2.3.4-p2'=> '1.1.4',
+                '2.3.5'=>    '1.1.5',
+                '2.3.5-p1'=> '1.1.5',
+                '2.3.5-p2'=> '1.1.5-p1',
+                '2.3.6'=>    '1.1.6',
+                '2.3.6-p1'=> '1.1.6',
+                '2.3.7'=>    '1.1.7',
+                '2.3.7-p1'=> '1.1.7',
+                '2.3.7-p2'=> '1.1.7',
+                '2.4.0'=>    '1.2.0',
+                '2.4.0-p1'=> '1.2.0-p1',
+                '2.4.1'=>    '1.2.1',
+                '2.4.1-p1'=> '1.2.1',
+                '2.4.2'=>    '1.2.2',
+                '2.4.2-p1'=> '1.2.2',
+                '2.4.2-p2'=> '1.2.2',
+                '2.4.3'=>    '1.2.3',
+                '2.4.3-p1'=> '1.2.3-p1'],
+        'pb'=>
+            ['2.3.1'=>   '1.0.0',
+                '2.3.2'=>    '1.0.1',
+                '2.3.2-p2'=> '1.0.3',
+                '2.3.3'=>    '1.1.0',
+                '2.3.3-p1'=> '1.1.1',
+                '2.3.4'=>    '1.2.0',
+                '2.3.4-p1'=> '1.2.1',
+                '2.3.4-p2'=> '1.2.2',
+                '2.3.5'=>    '1.3.0',
+                '2.3.5-p1'=> '1.3.1',
+                '2.3.5-p2'=> '1.3.2',
+                '2.3.6'=>    '1.3.3',
+                '2.3.6-p1'=> '1.3.3-p1',
+                '2.3.7'=>    '1.3.4',
+                '2.3.7-p1'=> '1.3.4',
+                '2.3.7-p2'=> '1.3.4-p1',
+                '2.4.0'=>    '1.4.0',
+                '2.4.0-p1'=> '1.4.1',
+                '2.4.1'=>    '1.5.0',
+                '2.4.1-p1'=> '1.5.1',
+                '2.4.2'=>    '1.6.0',
+                '2.4.2-p1'=> '1.6.0',
+                '2.4.2-p2'=> '1.6.0',
+                '2.4.3'=>    '1.7.0',
+                '2.4.3-p1'=> '1.7.0-p1'],
+        'security'=>
+                ['2.4.0'=>    '1.0.0',
+                '2.4.0-p1'=> '1.0.1',
+                '2.4.1'=>    '1.1.0',
+                '2.4.1-p1'=> '1.1.1',
+                '2.4.2'=>    '1.1.1',
+                '2.4.2-p1'=> '1.1.1',
+                '2.4.2-p2'=> '1.1.1',
+                '2.4.3'=>    '1.1.2',
+                '2.4.3-p1'=> '1.1.2']
+    ];
 
 $mode = 'git';
 $PWD = $_SERVER['PWD'];
@@ -51,64 +119,62 @@ if (empty($argv[1]) || empty($argv[2])) {
 
 $patchFile = $PWD . '/' . $argv[1];
 
+
 if (!preg_match('/\d+\.\d+\.\d+/', $argv[1]) && file_exists($patchFile)) {
     $mode = 'file';
 }
 
 $version = !empty($argv[3]) && preg_match('/\d+/', $argv[3], $matches) ? (int)$matches[0] : 1;
 
+
+//create repo dir
+if (!file_exists($repoPath)) {
+    mkdir($repoPath, 0777, true);
+}
+
+foreach ($repositories as [$repo, $path]){
+    if (!file_exists("$repoPath/$repo")) {
+        mkdir("$repoPath/$repo", 0777, true);
+        exec("git clone $path $repoPath/$repo");
+    }
+}
+
+$diffAll = NULL;
 if ($mode == 'git') {
     $diffA = $argv[1];
     $diffB = $argv[2];
 
-    exec("git checkout $diffA && cd ./magento2ee && git checkout $diffA");
-    exec("git checkout $diffB && cd ./magento2ee && git checkout $diffB");
+    foreach ($repositories as [$repo, $path]) {
+
+        if (in_array($repo, array('ce','ee'))){
+            $mVersion = $argv[1];
+        }
+        else{
+            $mVersion = $mapping[$repo][$diffA];
+        }
+        echo $mVersion;
+        exec("cd $repoPath'/'$repo && git pull origin && git checkout $mVersion");
+        exec("cd $repoPath'/'$repo && git checkout $diffB");
+        $isBranchExist = shell_exec("cd $repoPath'/'$repo && git branch | grep $diffB");
+        $diffRepo = $isBranchExist ? shell_exec("cd $repoPath'/'$repo && git diff $mVersion $diffB") : '';
+        $diffAll .= $diffRepo;
+    }
     $isBranchExistInCE = shell_exec("git branch | grep $diffB");
     $isBranchExistInEE = shell_exec("cd ./magento2ee && git branch | grep $diffB");
 
     $diffCE = $isBranchExistInCE ? shell_exec("git diff $diffA $diffB") : '';
     $diffEE = $isBranchExistInEE ? shell_exec("cd ./magento2ee && git diff $diffA $diffB") : '';
-    $diff = $diffCE . $diffEE;
+    $diff = $diffCE . $diffEE . $diffAll;
     $patchName = "{$diffB}_EE_{$diffA}_v$version";
-} elseif ($mode == 'file') {
-    $diff = file_get_contents($patchFile);
-    $withoutPrefixPatterns = array(
-        '/(\+\+\+|\-\-\-)\s(app|lib)/'
-    );
 
-    $diff = preg_replace_callback($withoutPrefixPatterns, function($matches){
-        $prefix = 'a/';
-        if ($matches[1] == '+++') {
-            $prefix = 'b/';
-        }
-        return "{$matches[1]} {$prefix}{$matches[2]}";
-    }, $diff);
-    $diffB = $argv[2];
-    $patchName = "{$diffB}_EE_v$version";
-}
-
-if ($diff) {
-    $replaceMap = array(
-        'app/code/Magento/' => 'vendor/magento/module-',
-        'lib/internal/Magento/' => 'vendor/magento/',
-        'app/design/frontend/Magento/' => 'vendor/magento/theme-frontend-',
-        'app/design/adminhtml/Magento/' => 'vendor/magento/theme-adminhtml-'
-    );
-
-    $patterns = array(
-        '/(app\/code\/Magento\/)([A-Za-z]+)\//',
-        '/(lib\/internal\/Magento\/)([A-Za-z]+)\//',
-        '/(app\/design\/frontend\/Magento\/)([A-Za-z]+)\//',
-        '/(app\/design\/adminhtml\/Magento\/)([A-Za-z]+)\//',
-    );
-
-    $diffComposer = preg_replace_callback($patterns, 'replaceCallback', $diff);
+    //remove 'AUTO_xxx' suffix
+    $patchName = preg_replace('/_AUTO_\d{1,}/', '', $patchName);
 
     $patchGitFilename = $patchName . '.patch';
     $patchComposerFilename = $patchName . '.composer.patch';
 
     file_put_contents('./' . $patchGitFilename, $diff);
-    file_put_contents('./' . $patchComposerFilename, $diffComposer);
+    $diffComposer = shell_exec( "convert-for-composer.php $patchGitFilename > $patchComposerFilename");
 
     echo "\n";
     echo "######################################################################\n";
@@ -120,5 +186,3 @@ if ($diff) {
     echo "# There is no available diff\n";
     echo "######################################################################\n";
 }
-
-//echo($diff);
