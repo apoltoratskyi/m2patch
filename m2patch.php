15a18a9465bdfca6e518e89b0db46cff54918027
Andrii Poltoratskyi <poltorat@adobe.com>
2023-08-23 13:38:01 -0700

    add .env.example file for configuration

--- m2patch.php
+++ m2patch.php
@@ -1,13 +1,20 @@
 #!/usr/bin/env php
 <?php
 
-define('BASE_PATH',realpath(__DIR__));
+// Load environment variables from .env file
+$envPath = __DIR__ . '/.env';
+if (file_exists($envPath)) {
+    $envVariables = parse_ini_file($envPath);
+} else {
+    echo "No .env file found. Make sure you have set up your environment variables.";
+    exit(1);
+}
 
-const GIT_TOKEN = '';
-const JIRA_HOST="";
-const JIRA_USER="";
-const JIRA_PASS="";
-const TEST_ON_CLOUD = "NO";
+// Jira custom fields
+$gitPullRequestField = 'customfield_13904';
+$projectPageField = 'customfield_18505';
+$environmentTypeField = 'customfield_17502';
+$gitRepo = 'magento-sparta';
 
 $PWD = $_SERVER['PWD'];
 $diffA = $diffB = false;
@@ -27,14 +34,14 @@ $newUrls = [];
 $urls = '';
 
 // get ticket info from jira
-function getTicket($ticketId) {
+function getTicket($ticketId, $envVariables) {
 // cURL initialization
-    $ch = curl_init(JIRA_HOST.'/rest/api/2/issue/'.$ticketId);
+    $ch = curl_init($envVariables['JIRA_HOST'].'/rest/api/2/issue/'.$ticketId);
 
 // Set cURL options
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    curl_setopt($ch, CURLOPT_USERPWD, JIRA_USER.":".JIRA_PASS);
+    curl_setopt($ch, CURLOPT_USERPWD, $envVariables['JIRA_USER'].":".$envVariables['JIRA_PASS']);
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 
 // Execute cURL request
@@ -59,29 +66,29 @@ function getVersion($response) {
 
 // get $urls of pull requests
 
-function getGitUrl($response) {
+function getGitUrl($response, $gitPullRequestField) {
 
-    return $response['fields']['customfield_13904'];
+    return $response['fields'][$gitPullRequestField];
 }
 
 // get project url
-function getProjectUrl($response) {
+function getProjectUrl($response, $projectPageField) {
 
-    return $response['fields']['customfield_18505'];
+    return $response['fields'][$projectPageField];
 }
 
 // get project env type (stg/prd)
-function getProjectType($response) {
-    return $response['fields']['customfield_17502']['value'];
+function getProjectType($response, $environmentTypeField) {
+    return $response['fields'][$environmentTypeField]['value'];
 }
 
-function convertToGitApi($pulls) {
+function convertToGitApi($pulls, $gitRepo) {
 
-    preg_match_all('#(https://)(github.com)(/magento-sparta/)(.+?)(/pull)(/\d+)#', $pulls, $match);
+    preg_match_all('#(https://)(github.com)(/'.$gitRepo.'/)(.+?)(/pull)(/\d+)#', $pulls, $match);
     foreach ($match[0] as $url) {
         if($url && strlen($url) > 10){
             echo $url . '       - included'. PHP_EOL;
-            $newUrl = preg_replace_callback('#(https://)(github.com)(/magento-sparta/)(.+?)(/pull)(/\d+)#', function($match) {
+            $newUrl = preg_replace_callback('#(https://)(github.com)(/'.$gitRepo.'/)(.+?)(/pull)(/\d+)#', function($match) {
                 return $match[1] . 'api.' . $match[2] . '/repos' . $match[3] . $match[4] . $match[5] . 's' . $match[6];
             }, $url);
             $newUrls[] = $newUrl;
@@ -90,13 +97,13 @@ function convertToGitApi($pulls) {
     return $newUrls;
 }
 
-function getPullRequestContent($pullRequests) {
+function getPullRequestContent($pullRequests, $envVariables) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $pullRequests);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch,CURLOPT_HTTPHEADER, array(
         "Accept: application/vnd.github.v3.diff",
-        "Authorization: Bearer " . GIT_TOKEN,
+        "Authorization: Bearer " . $envVariables['GIT_TOKEN'],
         "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
     ));
     $result = curl_exec($ch);
@@ -112,24 +119,23 @@ function sshUrl($projectPage, $projectType) {
 }
 
 // execute
-
-$response = getTicket($argv[1]);
+$response = getTicket($argv[1], $envVariables);
 $patchGitFilename = $argv[1] . "_" . getVersion($response) . $patchVersion . ".git.patch";
 $patchComposerFilename = $argv[1] . "_" . getVersion($response) . $patchVersion . ".patch";
 
-if (strlen(getGitUrl($response)) > 10) {
-    $newUrls = convertToGitApi(getGitUrl($response));
+if (strlen(getGitUrl($response,$gitPullRequestField)) > 10) {
+    $newUrls = convertToGitApi(getGitUrl($response, $gitPullRequestField), $gitRepo);
     foreach ($newUrls as $newUrl) {
-        file_put_contents($patchGitFilename, getPullRequestContent($newUrl), FILE_APPEND);
+        file_put_contents($patchGitFilename, getPullRequestContent($newUrl, $envVariables), FILE_APPEND);
     }
 }
 
 $patchComposer = shell_exec( "m2-convert-for-composer $patchGitFilename > $patchComposerFilename && rm $patchGitFilename");
 echo "Patch file:        -----------           " . $patchComposerFilename . "         ---------------            " .  PHP_EOL;
 
-if (TEST_ON_CLOUD === "YES") {
-    $sshLink = sshUrl(getProjectUrl($response), getProjectType($response));
-    echo ("Trying to apply the patch to:  $sshLink  ---  ".getProjectType($response)) . PHP_EOL;
+if ($envVariables['TEST_ON_CLOUD'] === "YES") {
+    $sshLink = sshUrl(getProjectUrl($response, $projectPageField), getProjectType($response, $environmentTypeField));
+    echo ("Trying to apply the patch to:  $sshLink  ---  ".getProjectType($response, $environmentTypeField)) . PHP_EOL;
     $patchApplicable = shell_exec ("cloud-patchcheck $sshLink $patchComposerFilename");
     echo $patchApplicable;
 }
